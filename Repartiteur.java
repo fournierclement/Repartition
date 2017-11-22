29fe2650b185d8cb5093ace3a79c7c13916f2a51
fournierclement <fournier.clt@gmail.com>
2017-11-22 14:26:36 +0100

    Update Repartiteur.java

--- Repartiteur.java
+++ Repartiteur.java
@@ -6,20 +6,24 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Repartiteur {
-
 	public void main( String[] arg){
+		Eleves[] eleves;
+		// Ce try/catch pourrait être encapsulé.
 		try {
-            Reader reader = Files.newBufferedReader(Paths.get(arg[0]));
-            CSVReader csvReader = new CSVReader(reader);
-    
-            // Reading Records One by One in a String array
-            String[] nextRecord;
-            while ((nextRecord = csvReader.readNext()) != null) {
-            }
-        } catch (IOException e) {
-        	System.err.println(e);
-        	System.exit(-1);
-        }
-		
+			Reader reader = Files.newBufferedReader(Paths.get(arg[0]));
+			CSVReader csvReader = new CSVReader(reader);
+			// Reading Records One by One in a String array
+			String[] currentLine = csvReader.readNext();
+			eleves = new Array(currentLine.length);
+			for(int i = 1; i < currentLline.length; i++ ){
+				eleves[i-1] = new Eleve(currentLine[i]);
+			}
+			while ((currentLine = csvReader.readNext()) != null) {
+				//copier coller le java de San Wei pour transformer les quantitatif en qualitatif
+			}
+		} catch (IOException e) {
+			System.err.println("Erreur: ce fichier n'existe pas ou ne respecte pas le former requis.");
+			System.exit(-1);
+		}
 	}
 }
